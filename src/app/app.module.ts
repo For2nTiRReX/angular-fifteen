3835f49924448a030d9b2afe40088bc1dfac1977
Boghdan <bogdkovalov@gmail.com>
2018-04-17 20:04:49 +0300

    g components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RootRoutingModule } from './root-routing.module';
 import {PlayGameComponent} from './play/play-game/play-game.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login/login.component';
+import { TopPlayersComponent } from './top-players/top-players.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayGameComponent
+    PlayGameComponent,
+    HomeComponent,
+    LoginComponent,
+    TopPlayersComponent
   ],
   imports: [
     RootRoutingModule,
---
+++ src/app/top-players/top-players.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fifteen-top-players',
+  templateUrl: './top-players.component.html',
+  styleUrls: ['./top-players.component.scss']
+})
+export class TopPlayersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
