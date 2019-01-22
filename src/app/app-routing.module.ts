2204f2573dad65c636fd3713e35306def198a756
Bohdan Kovalov <bohdan.kovalov@synapse.com>
2019-01-22 18:28:34 +0100

    fix lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from 'core-module/components/home/home.component';
 import { PlayerAuthGuardService } from 'shared-module/services/player-auth-guard.service';
 
-import { LoginModule } from 'login-module/login.module';
+//import { LoginModule } from 'login-module/login.module';
 import { GameModule } from 'game-module/game.module';
 import { StatisticModule } from 'statistic-module/statistic.module';
 
@@ -14,17 +14,14 @@ const routes: Routes = [
         component: HomeComponent
     },
     {
-        path: 'login', loadChildren: () => LoginModule
-        // './../login/login.module#LoginModule'
+        path: 'login', loadChildren: 'login-module/login.module#LoginModule'
     },
     {
-        path: 'game', loadChildren: () => GameModule,
+        path: 'game', loadChildren: 'game-module/game.module#GameModule',
         canActivate: [ PlayerAuthGuardService ]
-        // 'login-module/login.module#GameModule'
     },
     {
-        path: 'top-players', loadChildren: () => StatisticModule,
-        // 'login-module/login.module#GameModule'
+        path: 'top-players', loadChildren: 'statistic-module/statistic.module#StatisticModule'
     },
     {
         path: '',
